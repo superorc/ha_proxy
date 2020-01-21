bd2cfb8a323091ecc71f16340e8ed1e1c2fd3c89
superorc <rykanoga@gmail.com>
2020-01-21 20:54:33 +0300

    Set timouts in local test

--- run_local_test.sh
+++ run_local_test.sh
@@ -5,21 +5,25 @@
 set -oe pipefail
 
 # bootstrap local VM's via virtualbox
-cd tests && vagrant up node1 node2 #node3
+cd tests && timeout 5m vagrant up node1 node2 #node3
 # or just run all VMs with vagrant up
 
 # apply role
-ansible-playbook -i inventory-local test.yml -vv
+timeout 2m ansible-playbook -i inventory-local test.yml -vv
 
 # Idempotence check
 # Run the role/playbook again, checking to make sure it's idempotent.
 ansible-playbook -i inventory-local test.yml -vv | grep -q 'changed=0.*failed=0' \
   && (echo '******************************') \
-  && (echo '*** Idempotence test ***') \
+  && (echo 'Idempotence test') \
   && (echo '******************************') \
   && (echo 'passed' && exit 0) \
-  || (echo 'FAILED' && exit 1)
+  || (echo '******************************') \
+  || (echo 'FAILED' && exit 1) \
+  || (echo '******************************')
 
 # terminate tests VM's
 # uncomment below to destroy local VM's
 #vagrant destroy --force
+
+echo 'Done.. See results above'
